60b3856eb78489a0f0544e6cdba450e8cab286eb
xisingazix <xisingazix@hotmail.com>
2026-02-04 09:46:48 +0800

    redemption updates

--- assets/profile.js
+++ assets/profile.js
@@ -1,13 +1,15 @@
 // js for profile.html 
-const isLoggedIn = localStorage.getItem("isLoggedIn"); // check local storage if user is logged in
-const username = localStorage.getItem("username"); 
+const isLoggedIn = localStorage.getItem("usertoken"); // check local storage if user is logged in
+
 
 if (!isLoggedIn) {
     window.location.href = "signuplogin.html";  // if not log in to return user to log in page
 }
+function displayName(username){
+   return username.split('@')[0];
+}
+document.getElementById("profile-username").textContent = displayName(username); // to be confirmed id of elemen
 
-document.getElementById("profile-username").textContent = username; // to be confirmed id of elemen
-
-document.getElementById("profileNameContainer").addEventListener('click', function(){
+document.getElementById("profileContainer").addEventListener('click', function(){
     window.location.href = "/profile.html";
 })
