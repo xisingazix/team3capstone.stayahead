301b04979dfce1da34fd3f9c3dafae292a568e91
xisingazix <xisingazix@hotmail.com>
2026-02-04 12:08:57 +0800

    mobile nav profile updated

--- script/loader.js
+++ script/loader.js
@@ -1,6 +1,6 @@
 let spinner = null;
-function displayName(email){
-   return email.split('@')[0];
+function displayName(email) {
+    return email.split('@')[0];
 }
 
 // EventListener to instantiate the navController
@@ -11,53 +11,56 @@ document.addEventListener("DOMContentLoaded", async (event) => {
      * *********************************************************************
     */
     spinner = new Spinner();
-    
+
     /*
     * *********************************************************************
     * Display or hider userProfile link.
     * *********************************************************************
     */
 
-    const token = isAuthenticated();   
-    
+    const token = isAuthenticated();
+
     const profileContainer = document.getElementById("profileContainer");
     const profileNameContainer = document.getElementById("profileNameContainer");
     const loginLink = document.getElementById("loginLink");
     const logoutLink = document.getElementById("logoutLink");
     const m_loginLink = document.getElementById("m_loginLink");
     const m_logoutLink = document.getElementById("m_logoutLink");
-    
-    if(!token){
+    const m_profileContainer = document.getElementById("m_profileContainer");
+    const m_profileNameContainer = document.getElementById("m_profileNameContainer");
+
+    if (!token) {
         // User is not logged in
-        profileContainer.classList.add("d-none");
+        m_profileContainer, profileContainer.classList.add("d-none");
         // desktop view
         loginLink.classList.remove("d-none");
         logoutLink.classList.add("d-none");
         // mobile view
         m_loginLink.classList.remove("d-none");
         m_logoutLink.classList.add("d-none");
-        
+
     }
-    else{
+    else {
         // User is logged in
         const user = decodeUser(token);
         profileNameContainer.textContent = displayName(user.email);
-        profileContainer.style.cursor = "pointer";
+        m_profileNameContainer.textContent = displayName(user.email);
         // desktop view
         loginLink.classList.add("d-none");
         logoutLink.classList.remove("d-none");
         logoutLink.addEventListener("click", () => {
-          logout();
+            logout();
         })
         // mobile_view
         m_loginLink.classList.add("d-none");
         m_logoutLink.classList.remove("d-none");
         m_logoutLink.addEventListener("click", () => {
-          logout();
+            logout();
         })
     }
+    m_profileContainer,profileContainer.addEventListener('click', function () {
+        window.location.href = "/profile.html";
+    })
+    profileContainer.style.cursor = "pointer";
 });
 
-document.getElementById("profileContainer").addEventListener('click', function(){
-    window.location.href = "/profile.html";
-})
